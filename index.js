f2d17b54fccb8e40053d9a2a6222b97773e972cf
dorothymuhonja <dorothymuhonja7@gmail.com>
2022-06-21 00:36:58 +0300

    commit tree 9fdb18b37fac2b929c75ab4e5c752ca24977fe37

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+cats.push()
+
+function destructivelyAppendCat() {
+    return cats.push("Ralph");
+    
+}
+destructivelyAppendCat();
+
+function destructivelyPrependCat() {
+    return cats.unshift("Bob");
+    
+}
+destructivelyPrependCat();
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+    
+}
+destructivelyRemoveLastCat();
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+    
+}
+destructivelyRemoveFirstCat();
+
+function appendCat() {
+    const newCat = [...cats, "Broom"]
+    return newCat
+
+}
+appendCat()
+
+function prependCat() {
+    const newCat = ["Arnold", ...cats]
+    return newCat
+
+}
+prependCat()
+
+function removeLastCat() {
+    const removeCat = cats.slice(0, 2);
+    return removeCat
+}
+removeLastCat()
+
+function removeFirstCat() {
+    const removeNeko = cats.slice(1);
+    return removeNeko
+}
+removeFirstCat()
